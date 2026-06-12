60b7db218047748a7e7dbda41ec1d6d93c93aa7b
Saeedha Nazar <saeedha@pekoe.ai>
2026-06-12 22:08:17 +0530

    bug fixes

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -430,7 +430,7 @@ class LoadStreams:
 
 def img2label_paths(img_paths):
     # Define label paths as a function of image paths
-    sa, sb = f'{os.sep}images{os.sep}', f'{os.sep}labels{os.sep}'  # /images/, /labels/ substrings
+    sa, sb = f'{os.sep}Images{os.sep}', f'{os.sep}Labels{os.sep}'  # /images/, /labels/ substrings
     return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
 
 
