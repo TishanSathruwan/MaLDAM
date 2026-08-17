d073bb5ca63e96b613d368f42323c947259ef084
Tishan Sathruwan <rathnasekaratishan@gmail.com>
2026-08-17 20:58:44 +0530

    minor update

--- src/config.py
+++ src/config.py
@@ -3,7 +3,6 @@ Copyright 2026 MaLDAM Authors. All Rights Reserved.
 
 MaLDAM: Masked Localized Domain Adaptation for Malaria Detection in Low-Cost Microscopic Images.
 Authors: Muditha Fernando, Tishan Rathnasekara, Saeedha Nazar, Avishka Perera, Tharindu Kaluarachchi
-Publication: HemaRAI 2026 | CC BY 4.0
 File: Configuration loading & saving
 """
 
